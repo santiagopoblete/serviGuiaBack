cdc17f58282c5e6e26a7ab65b8f43acc0e346530
FernandaGranadosMonge <A01252895@tec.mx>
2026-04-25 21:30:48 -0700

    ADD: AI output is now always in solicited format

--- classes.py
+++ classes.py
@@ -1,5 +1,21 @@
 from pydantic import BaseModel
+from enum import Enum
 
 class UserInput(BaseModel):
     text: str
     image_url: str
+
+class UrgencyLevel(str, Enum):
+    CRITICO = "CRÍTICO"
+    MODERADO = "MODERADO"
+    NORMAL = "NORMAL"
+
+class AIResponse(BaseModel):
+    nivel_urgencia: UrgencyLevel
+    es_emergencia: bool
+    accion_inmediata: str | None = None # Solo proveer si la urgencia es crítica o moderada
+    categoria_detectada: str | None = None # Es necesario poner las categorías disponibles
+    pregunta_seguimiento: str | None = None # Solo proveer si el input del usuario es ambiguo
+    resumen_diagnostico: str
+    numero_emergencia: str | None = None
+    # proveedores_sugeridos será agregado después de correr el sistema de recomendación
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from openai import OpenAI
 
 from classes import UserInput
+from classes import AIResponse
 from chat_functions import build_content
 
 load_dotenv()
@@ -19,26 +21,19 @@ async def read_root():
 async def user_question(input: UserInput):
     content = build_content(input)
 
-    response = client.responses.create(
+    response = client.responses.parse(
         model="gpt-5-nano",
+        # instructions="Eres un asistente de deteccion de emergencias en el hogar. Si la situación es una emergencia, identifícala. Si no lo es, clasifícala en una categoría de acuerdo al servicio requerido para solucionarla.", #! Aquí irá el master prompt.
         input=[
-            # {
-            #     "role": "system",
-            #     "content": [
-            #         {
-            #             "type": "input_text",
-            #             "text": "Eres un asistente virtual llamado ServiGuia ..." #! AQUÍ VA EL MASTER PROMPT
-            #         }
-            #      ]
-            # },
             {
                 "role": "user",
                 "content": content
             }
         ],
+        text_format=AIResponse,
     )
 
     # TODO: Aquí se llamará a la función que se haga que devuelva la lista de proveedores a mostrar (NO LA LLAMADA A LA BASE DE DATOS,
     # TODO:  SINO YA DESPUES DE ESCOGER LAS RECOMENDACIONES DE PROVEEDORES) y se agregará a la respuesta del modelo (response.output_text).
 
-    return response.output_text # TODO: Mientras se hace la función anterior, se devuelve la respuesta del modelo, pero debe regresar la lista de proveedores.
+    return response.output_parsed # TODO: Mientras se hace la función anterior, se devuelve la respuesta del modelo, pero debe regresar la lista de proveedores tambien.
