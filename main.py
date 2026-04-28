f50eba73815cb32c1fa2ba8a64e39c43610da997
Santiago Eduardo Poblete Talamante <a01254609@tec.mx>
2026-04-27 19:00:11 -0700

    rate limiting

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
-import os
+import os 
 import asyncio
 import json
 
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from openai import OpenAI
 
-from classes.chat_classes import UserInput
-from classes.chat_classes import AIResponse
+from slowapi import Limiter , _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.errors import RateLimitExceeded
+
+from classes.chat_classes import UserInput, AIResponse
 from functions.chat_functions import build_content, load_master_prompt
 from functions.weight_functions import load_workers_from_db, output_workers
 from database import get_db
 
 load_dotenv()
 
-
 app = FastAPI()
 client = OpenAI()
 MASTER_PROMPT = load_master_prompt()
+
+limiter = Limiter(key_func=get_remote_address)
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
+
 
 @app.get("/")
 async def read_root():
     return {"ServiGuia x Equipo de Cuatro"}
 
+
 @app.post("/chat")
-async def user_question(input: UserInput):
+@limiter.limit("3/minute")
+async def user_question(request: Request, input: UserInput):
     content = build_content(input)
 
     response = client.responses.parse(
@@ -39,23 +52,23 @@ async def user_question(input: UserInput):
         text_format=AIResponse,
     )
 
-    # TODO: Aquí se llamará a la función que se haga que devuelva la lista de proveedores a mostrar (NO LA LLAMADA A LA BASE DE DATOS,
-    if not response.output_parsed.es_emergencia and not (response.output_parsed.pregunta_seguimiento or response.output_parsed.pregunta_necedidades_usuario):
+    if not response.output_parsed.es_emergencia and not (
+        response.output_parsed.pregunta_seguimiento or 
+        response.output_parsed.pregunta_necedidades_usuario
+    ):
         user_needs = response.output_parsed.necesidades_usuario
     
         workers = await load_workers_from_db()
 
-        filtered = output_workers(workers, user_needs.category, user_needs.user_expected_expertise, (user_needs.user_price_range.min, user_needs.user_price_range.max))
-                
-
-
+        filtered = output_workers(
+            workers,
+            user_needs.category,
+            user_needs.user_expected_expertise,
+            (user_needs.user_price_range.min, user_needs.user_price_range.max)
+        )
 
         response.output_parsed.proveedores_sugeridos = filtered
-
     else:
         response.output_parsed.proveedores_sugeridos = None
 
-
-    # TODO:  SINO YA DESPUES DE ESCOGER LAS RECOMENDACIONES DE PROVEEDORES) y se agregará a la respuesta del modelo (response.output_text).
-
-    return response.output_parsed # TODO: Mientras se hace la función anterior, se devuelve la respuesta del modelo, pero debe regresar la lista de proveedores tambien.
+    return response.output_parsed
